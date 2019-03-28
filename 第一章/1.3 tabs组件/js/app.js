593bf3f515fc58c9d431bcfe9ede0acc0d0b24bb
Hilay <1529823869@qq.com>
2019-03-28 09:42:32 +0800

    1.3

---
+++ 第一章/1.3 tabs组件/js/app.js
@@ -0,0 +1,13 @@
+import React,{Component, PropTypes} from "react";
+class Tabs extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return <div className="ui-tabs"></div>;
+    }
+};
+
+export default Tabs;
